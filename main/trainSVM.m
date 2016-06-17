a875557051873f144c0d32241ab2706c109546b9
shawn <shawn@mirlabde-Mac-mini.local>
2016-06-18 00:30:59 +0800

    add validation to trainSVM.m

--- main/trainSVM.m
+++ main/trainSVM.m
@@ -1,6 +1,7 @@
-
-load('gabor_W_4_O_90_KDEF_features.mat');
-load('gabor_W_4_O_90_KDEF_label.mat');
+load('KDEF_features.mat');
+load('KDEF_label.mat');
+% load('gabor_W_4_O_90_KDEF_features.mat');
+% load('gabor_W_4_O_90_KDEF_label.mat');
 
 feature = trainingFeature;
 
@@ -11,13 +12,16 @@ for i=1:size(trainingLabel,2)
     label(i, 1) = M(trainingLabel{i});
 end
 
-option = '-t 0 -d 2 -g 2 -c 1 -e 0.1';
-model = svmtrain(double(label), double(feature), option);
+randInd = randperm(size(label,1));
+trainRainge = size(label)-100;
+trainInd = randInd(1:trainRainge);
+valInd = randInd(trainRainge+1:end);
 
-[predicted_label] = svmpredict(double(label), double(feature), model);
+option = '-t 0 -d 2 -g 2 -c 1 -e 0.1';
+model = svmtrain(double(label(trainInd)), double(feature(trainInd,:)), option);
+
+[predicted_label, accuracy, ~] = svmpredict(double(label(valInd)), double(feature(valInd,:)), model);
 
 % C = strsplit(option, {' ','-','.'});
 % par = strcat(C{:});
 % save(['model_MIX_' par '.mat'],'model_MIX');
-
-
