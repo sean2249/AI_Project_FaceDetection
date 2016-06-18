e8fa6411fdd5c1d538b83b08d873f18046f6a334
sean2249 <sean22492249@gmail.com>
2016-06-18 18:03:15 +0800

    add bmp file

--- main/refFunction/refGabor.m
+++ main/refFunction/refGabor.m
@@ -1,7 +1,7 @@
 % I = imread('board.tif');
 % I = imread('./train_KDEF/happy');
-folder = dir('./train_KDEF/happy/*.jpg');
-I = imread(strcat('./train_KDEF/happy/', folder(1).name));
+folder = dir('../train_KDEF/happy/*.jpg');
+I = imread(strcat('../train_KDEF/happy/', folder(1).name));
 I = rgb2gray(I);
 
 wavelength = 2;
@@ -9,10 +9,7 @@ orientation = 0;
 [mag, phase] = imgaborfilt(I, wavelength, orientation);
 % mag = imgaussfilt(I);
 
-% figure; imshow(I); title('Original');
-% figure; imshow(mag, []); title('Gabor Magnitude');
-% figure; imshow(phase, []); title('Gabor Phase');
-
-for wavelength=1:10
-    for orientation = 
+figure; imshow(I); title('Original');
+figure; imshow(mag, []); title('Gabor Magnitude');
+figure; imshow(phase, []); title('Gabor Phase');
 
